d534175be108e85719607a6616161e4c1b0eac6d
Oh-Jun-Seok <oh29544@gmail.com>
2021-10-07 21:07:09 +0900

    완료
    opening 유저 이름 받기 챕터버튼  완료

---
+++ Space1945/Assets/Scripts/PanelListeners/NamePanelListener.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NamePanelListener : MonoBehaviour
+{
+    public InputField name_field;
+    public Button submit_btn;
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+    public void Initiate()
+    {
+        PlayerPrefs.SetString("pilot_name", name_field.text);
+        PlayerPrefs.SetInt("pilot_level", 1);
+        PlayerPrefs.SetInt("pilot_exp", 0);
+        PlayerPrefs.SetString("pilot_skill", "100000000000000000000000000000000000000000000"); // ±Ê¿Ã 45
+        PlayerPrefs.SetInt("gold", 10000);
+        PlayerPrefs.SetInt("max_level", 30);
+        PlayerPrefs.SetInt("max_exp", 200);
+
+        PlayerPrefs.SetString("chapter1", "10000");
+        PlayerPrefs.SetString("chapter2", "00000");
+        PlayerPrefs.SetString("chapter3", "00000");
+        PlayerPrefs.SetString("chapter4", "00000");
+        PlayerPrefs.SetString("bgm", "true");
+        PlayerPrefs.SetString("es", "true");
+        PlayerPrefs.SetString("refresh", "20200101120000");
+
+        PlayerPrefs.SetString("Stage1_1Type", "Robot");
+        PlayerPrefs.SetString("Stage1_1NormalSeq", "12121");
+        PlayerPrefs.SetString("Stage1_1NormalTime", "22222");
+        PlayerPrefs.SetString("Stage1_1Elite", "1");
+        PlayerPrefs.SetString("Stage1_5Boss", "1");
+
+        PlayerPrefs.SetString("Atk1", "using");
+        PlayerPrefs.SetString("Ship1", "using");
+        gameObject.SetActive(false);
+    }
+}
--- Space1945/Assets/Scripts/PanelListeners/OpeningListener.cs
+++ Space1945/Assets/Scripts/PanelListeners/OpeningListener.cs
@@ -7,45 +7,19 @@ using UnityEngine.SceneManagement;
 public class OpeningListener : MonoBehaviour
 {
     public Button main_button;
+    public GameObject name_panel;
 
     public void ClickedAnywhere()
     {
         if (!PlayerPrefs.HasKey("pilot_name"))
         {
-            PlayerPrefs.SetString("pilot_name", "user");
-            Initiate();
+            name_panel.SetActive(true);
         }
-        LoadScene();
+        else
+            LoadScene();
     }
     private void LoadScene()
     {
         SceneManager.LoadScene("Menu");
-    }
-
-    private void Initiate()
-    {
-        PlayerPrefs.SetInt("pilot_level", 1);
-        PlayerPrefs.SetInt("pilot_exp", 0);
-        PlayerPrefs.SetString("pilot_skill", "100000000000000000000000000000000000000000000"); // ±Ê¿Ã 45
-        PlayerPrefs.SetInt("gold", 10000);
-        PlayerPrefs.SetInt("max_level", 30);
-        PlayerPrefs.SetInt("max_exp", 200);
-
-        PlayerPrefs.SetString("chapter1", "10000");
-        PlayerPrefs.SetString("chapter2", "00000");
-        PlayerPrefs.SetString("bgm", "true");
-        PlayerPrefs.SetString("es", "true");
-        PlayerPrefs.SetString("refresh", "20200101120000");
-
-        PlayerPrefs.SetString("Stage1_1Type", "Robot");
-        PlayerPrefs.SetString("Stage1_1Normals", "Normal1 Normal2");
-        PlayerPrefs.SetString("Stage1_1NormalsTime", "100 100 100 100 100");
-        PlayerPrefs.SetString("Stage1_1Elites", "Elite1");
-        PlayerPrefs.SetInt("Stage1_1ElitesEmerCnt", 10);
-        PlayerPrefs.SetInt("Stage1_1Score", 0);
-        PlayerPrefs.SetString("Stage1_5Boss", "Boss1");
-
-        PlayerPrefs.SetString("Atk1", "using");
-        PlayerPrefs.SetString("Ship1", "using");
     }
 }
